c9e707ab3b1b2ed0e697dccdcfd11401c55edee5
jae-shin <jae5464@gmail.com>
2016-06-13 19:42:08 -0700

    Add BouncyDancer class

---
+++ src/bouncyDancer.js
@@ -0,0 +1,45 @@
+var BouncyDancer = function(top, left, timeBetweenSteps, speedX, speedY, height, width) {
+  this.speedX = speedX || 4;
+  this.speedY = speedY || 4;
+  this.height = height || 0;
+  this.width = width || 0;
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<img src="images/legoguy.png" class="bouncy-dancer"></img>');
+};
+
+BouncyDancer.prototype = Object.create(Dancer.prototype);
+BouncyDancer.prototype.constructor = BouncyDancer;
+
+BouncyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this);
+
+  var maxY = $('body').height();
+  var maxX = $('body').width();
+  // calculate the current position of the bouncyDancer
+  // update its position on the page
+  var newY = this.top + this.speedY;
+  var newX = this.left + this.speedX;
+
+  if (newX - this.width / 2 < 0) {
+    this.speedX = 0 - this.speedX;
+    this.left = 0 + this.width / 2;
+  } else if (newX + this.width / 2 > maxX) {
+    this.speedX = 0 - this.speedX;
+    this.left = maxX - this.width / 2;
+  } else {
+    this.left = newX;
+  }
+
+  if (newY - this.height / 2 < 0) {
+    this.speedY = 0 - this.speedY;
+    this.top = 0 + this.height / 2;
+  } else if (newY + this.height / 2 > maxY) {
+    this.speedY = 0 - this.speedY;
+    this.top = maxY - this.height / 2;
+  } else {
+    this.top = newY;
+  }
+
+  this.setPosition(this.top, this.left);
+};
