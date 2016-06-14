93754afe8041e2d183834ede927be4a362676c57
jae-shin <jae5464@gmail.com>
2016-06-13 21:03:09 -0700

    Implement easing function in casualDancer for more natural movement

--- src/casualDancer.js
+++ src/casualDancer.js
@@ -1,7 +1,9 @@
-var CasualDancer = function(top, left, timeBetweenSteps, speed, range) {
-  this.xOffset = 0;
-  this.speed = speed || 2;
-  this.range = range || 300;
+var CasualDancer = function(top, left, timeBetweenSteps, range) {
+  this.range = range || 750;
+  this.d = 200;
+  this.progress = 0;
+  this.increment = -1;
+
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node = $('<img src="images/casualpenguin.gif" class="casual-dancer"></img>');
 };
@@ -12,10 +14,27 @@ CasualDancer.prototype.constructor = CasualDancer;
 CasualDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
 
-  if (this.xOffset + this.speed > this.range || this.xOffset + this.speed < -this.range) {
-    this.speed = -this.speed;
-  }
-  this.xOffset += this.speed;
+  var easeInOutCubic = function (t, b, c, d) {
+    if ((t /= d / 2) < 1) {
+      return c / 2 * t * t * t + b;
+    }
+    return c / 2 * ((t -= 2) * t * t + 2) + b;
+  };
 
-  this.setPosition(this.top, this.left + this.xOffset);
+  if (this.progress === this.d || this.progress === 0) {
+    this.increment = -this.increment;
+  }
+
+
+  this.progress += this.increment;
+  var xOffset = easeInOutCubic(this.progress, this.left, this.range, this.d);
+
+  this.setPosition(this.top, xOffset);
 };
+
+
+
+
+
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
